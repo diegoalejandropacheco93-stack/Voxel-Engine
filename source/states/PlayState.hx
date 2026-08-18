4eec67f1891d6965da2355ec15cbe73814d85ea3
PachecoUITU <diegoalejandropacheco93@gmail.com>
2026-08-18 15:08:08 -0400

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1,19 +1,4 @@
-package states;
-
-import flixel.FlxG;
-import flixel.FlxSprite;
-import flixel.tweens.FlxTween;
-import flixel.tweens.FlxEase;
-import backend.ClientPrefs;
-import objects.Note;
-import objects.StrumNote;
-import objects.NoteSplash;
-import psychlua.LuaUtils;
-import stages.BaseStage;
-
-class PlayState extends MusicBeatState
-{
-	private function keysCheck():Void
+private function keysCheck():Void
 	{
 		// HOLDING
 		var holdArray:Array<Bool> = [];
@@ -375,4 +360,3 @@ class PlayState extends MusicBeatState
 		if (scoreTxt != null) scoreTxt.alpha = ClientPrefs.data.hudAlpha;
 		if (timeBar != null) timeBar.visible = (ClientPrefs.data.timeBarType != 'Disabled') && !ClientPrefs.data.hideHud;
 	}
-}
